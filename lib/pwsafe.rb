5d49b03f8f078fec4c8d95ecb9537557525c4bd8
Nurahmadie <nurahmadie@gmail.com>
2018-08-24 15:39:33 +0700

    Move around classes

--- lib/pwsafe.rb
+++ lib/pwsafe.rb
@@ -1,21 +1,7 @@
 require "pwsafe/version"
 
 module Pwsafe
-  class DecodeError < ArgumentError
-    class << self
-      def wrong_type(classname)
-        DecodeError.new("Wrong file type for #{classname}")
-      end
-
-      def file_corrupt(classname)
-        DecodeError.new("File likely corrupted for #{classname}")
-      end
-
-      def not_authenticated
-        DecodeError.new("need to check password first")
-      end
-    end
-  end
 end
 
+require "pwsafe/decode_error"
 require "pwsafe/v3"
---
+++ lib/pwsafe/decode_error.rb
@@ -0,0 +1,15 @@
+class Pwsafe::DecodeError < ArgumentError
+  class << self
+    def wrong_type(classname)
+      DecodeError.new("Wrong file type for #{classname}")
+    end
+
+    def file_corrupt(classname)
+      DecodeError.new("File likely corrupted for #{classname}")
+    end
+
+    def not_authenticated
+      DecodeError.new("need to check password first")
+    end
+  end
+end
